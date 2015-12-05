5e86b3191733922359678bec1849f2df90a0a98e
Tyler Martinez <slightlytyler@gmail.com>
2015-12-05 13:53:50 -0600

    Ok FS is working, time for imagemagick

--- app/pods/index/component.js
+++ app/pods/index/component.js
@@ -2,20 +2,29 @@ import React, { Component } from 'react';
 import { Link } from 'react-router';
 import Radium from 'radium';
 
+import fs from 'fs';
+import path from 'path';
+
 @Radium
 export default class IndexComponent extends Component {
+  test() {
+    //var stats = fs.statSync("/pods/index/input.jpg");
+    let stats = fs.lstatSync('./app/pods/index/input.jpg');
+    console.log(stats.isFile());
+
+    fs.writeFile('./app/pods/index/test.txt', 'Hello World!', function (err) {
+      if (err) return console.log(err);
+      console.log('Hello World > helloworld.txt');
+    });
+
+  }
+
   render() {
     return (
-      <div>
-        <div style={styles.container}>
-          <h2 style={styles.header}>Home</h2>
-          <Link
-            to="/counter"
-            style={styles.link}
-          >
-            to Counter
-          </Link>
-        </div>
+      <div style={styles.container}>
+        <h1>Index</h1>
+
+        <span onClick={() => this.test()}>Click Me</span>
       </div>
     );
   }
