c7998c801fec78ab2bc5de338ae3255c31ce4bbd
Tyler Martinez <slightlytyler@gmail.com>
2015-12-06 13:56:43 -0600

    Basics of selecting and processing a file

--- app/pods/index/component.js
+++ app/pods/index/component.js
@@ -5,22 +5,29 @@ import Radium from 'radium';
 import fs from 'fs';
 import gm from 'gm';
 
+import FileInput from 'react-file-input';
+
 @Radium
 export default class IndexComponent extends Component {
-  test() {
-    const baseImage = gm('./app/pods/index/input.jpg');
-    const overlayImage = gm('./app/pods/index/overlay.png').geometry(100, 100);
-
+  test(path) {
+    console.log(process.cwd());
+    let stats = fs.lstatSync(path);
+    console.log(stats.isFile());
     gm()
     .in('-page', '+0+0')
     .in('./app/pods/index/input.png')
     .in('-geometry','420x748')
     .in('-page', '+28+117')
-    .in('./app/pods/index/overlay.png')
+    .in(path)
     .mosaic()
     .write('./app/pods/index/output.png', function(err) {
       if(!err) console.log("Written composite image.");
     });
+  }
+
+  handleFile(event) {
+    console.log(event.target.files[0]);
+    this.test(event.target.files[0].path);
   }
 
   render() {
@@ -28,7 +35,11 @@ export default class IndexComponent extends Component {
       <div style={styles.container}>
         <h1>Index</h1>
 
-        <span onClick={() => this.test()}>Click Me</span>
+        <span onClick={this.test}>Click Me</span>
+
+        <FileInput name="screenshot"
+                   placeholder="Select Screenshot"
+                   onChange={(e) => this.handleFile(e)} />
       </div>
     );
   }
