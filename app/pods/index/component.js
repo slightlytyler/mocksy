094c5eb6ffaa2800a31a7999edefee2892883ca8
Tyler Martinez <slightlytyler@gmail.com>
2015-12-07 00:37:20 -0600

    Built out export panel

--- app/pods/index/component.js
+++ app/pods/index/component.js
@@ -58,27 +58,88 @@ export default class IndexComponent extends Component {
 
           <section
             className="export-panel"
-            style={styles.sidebar.exportPanel}
+            style={styles.sidebar.exportPanel.base}
           >
-            <header>
-              <span>
+            <header
+              style={[
+                styles.sidebar.exportPanel.section,
+                styles.sidebar.exportPanel.header.base
+              ]}
+            >
+              <span style={styles.sidebar.exportPanel.header.text}>
                 Export
               </span>
 
-              <img />
+              <img
+                src="assets/icons/add-size.svg"
+                style={styles.sidebar.exportPanel.header.addSizeIcon}
+              />
             </header>
 
-            <ul>
-              <li>
-                <input />
-                <input />
-                <input />
-                <img />
+            <ul
+              style={[
+                styles.sidebar.exportPanel.section,
+                styles.sidebar.exportPanel.sizeList.base,
+              ]}
+            >
+              <li style={styles.sidebar.exportPanel.sizeList.item}>
+                <div style={[
+                  styles.sidebar.exportPanel.sizeList.item.input.container,
+                  styles.sidebar.exportPanel.sizeList.item.size
+                ]}>
+                  <input
+                    style={styles.sidebar.exportPanel.sizeList.item.input.base}
+                  />
+
+                  <label style={styles.sidebar.exportPanel.sizeList.item.input.label}>
+                    Size
+                  </label>
+                </div>
+
+                <div style={[
+                  styles.sidebar.exportPanel.sizeList.item.input.container,
+                  styles.sidebar.exportPanel.sizeList.item.suffix
+                ]}>
+                  <input
+                    style={styles.sidebar.exportPanel.sizeList.item.input.base}
+                  />
+
+                  <label style={styles.sidebar.exportPanel.sizeList.item.input.label}>
+                    Suffix
+                  </label>
+                </div>
+
+                <div style={[
+                  styles.sidebar.exportPanel.sizeList.item.input.container,
+                  styles.sidebar.exportPanel.sizeList.item.format
+                ]}>
+                  <input
+                    style={styles.sidebar.exportPanel.sizeList.item.input.base}
+                  />
+
+                  <label style={styles.sidebar.exportPanel.sizeList.item.input.label}>
+                    Format
+                  </label>
+                </div>
+
+                <div
+                  style={styles.sidebar.exportPanel.sizeList.item.remove}
+                >
+                  <img
+                    style={styles.sidebar.exportPanel.sizeList.item.remove.icon}
+                    src="assets/icons/remove-size.svg"
+                  />
+                </div>
               </li>
             </ul>
 
-            <div>
-              <button>
+            <div
+              style={[
+                styles.sidebar.exportPanel.section,
+                styles.sidebar.exportPanel.actions.base
+              ]}
+            >
+              <button style={styles.sidebar.exportPanel.actions.export}>
                 Export Screenshot
               </button>
             </div>
@@ -159,12 +220,115 @@ const styles={
     },
 
     exportPanel: {
+      base: {
+        dispay: 'flex',
+        flexDirection: 'column',
+        backgroundColor: '#EDF3F5',
+      },
 
+      section: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        paddingLeft: '.75em',
+        paddingRight: '.75em',
+        borderBottom: '1px solid #CFD8DC',
+      },
+
+      header: {
+        base: {
+          paddingTop: '.25em',
+          paddingBottom: '.25em',
+        },
+
+        text: {
+          fontSize: '1.25em',
+          fontWeight: 400,
+          color: '#263238'
+        },
+
+        addSizeIcon: {
+          width: '1.25em'
+        }
+      },
+
+      sizeList: {
+        base: {
+          display: 'flex',
+          flexDirection: 'column',
+          paddingTop: '.5em',
+          paddingBottom: '.5em',
+        },
+
+        item: {
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+
+          input: {
+            base: {
+              width: '100%',
+              marginBottom: '.25em'
+            },
+
+            container: {
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              marginRight: '2em'
+            },
+
+            label: {
+              fontSize: '.8em'
+            }
+          },
+
+          size: {
+            flex: 5,
+          },
+
+          suffix: {
+            flex: 3,
+          },
+
+          format: {
+            flex: 3,
+          },
+
+          remove: {
+            alignSelf: 'flex-start',
+            marginTop: '.25em',
+
+            icon: {
+              width: '1.25em'
+            }
+          }
+        }
+      },
+
+      actions: {
+        base: {
+          display: 'flex',
+          paddingTop: '.5em',
+          paddingBottom: '.5em',
+        },
+
+        export: {
+          flex: 1,
+          color: '#607D8B',
+          border: '1px solid #607D8B',
+          borderRadius: '4px',
+          backgroundColor: 'transparent'
+        }
+      }
     },
   },
 
   content: {
     base: {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
       flex: 10
     }
   }
