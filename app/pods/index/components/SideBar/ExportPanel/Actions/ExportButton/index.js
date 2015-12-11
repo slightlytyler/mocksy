bc7e3917167cf1cfc653a713ef0469b2d1e59a67
Tyler Martinez <slightlytyler@gmail.com>
2015-12-10 20:29:00 -0600

    Added export-assets to api

--- app/pods/index/components/SideBar/ExportPanel/Actions/ExportButton/index.js
+++ app/pods/index/components/SideBar/ExportPanel/Actions/ExportButton/index.js
@@ -2,14 +2,9 @@
 
 import React, { Component, PropTypes } from 'react';
 import Radium from 'radium';
-import path from 'path';
 
-import { app, dialog } from 'remote';
-import { forEach } from 'lodash';
-
+import exportAssets from 'api/export-assets';
 import colors from 'constants/colors';
-import compositeImages from 'api/composite-images';
-import writeFile from 'api/write-file.js'
 
 
 @Radium
@@ -20,65 +15,17 @@ export default class IndexSideBarExportPanelExportButton extends Component {
     sizes: PropTypes.object.isRequired
   };
 
-  export() {
+  render() {
     const {
       currentTemplate,
       screenshot,
       sizes
     } = this.props;
-    const { foreground } = currentTemplate.dimensions;
-
-    const template = path.join(app.getAppPath(), `app/assets/base-templates/${currentTemplate.id.toLowerCase()}/template.png`);
-
-    // Get destination
-    dialog.showSaveDialog(fullDestination => {
-      const parsedDestination = path.parse(fullDestination);
-      const destination = path.join(parsedDestination.dir, parsedDestination.name);
-
-      // Build composites for each size
-      forEach(sizes, size => {
-        let {
-          id,
-          multiplier,
-          suffix,
-          format
-        } = size;
-
-        // Remove 'x' from multiplier and convert to number
-        let computedMultipler = multiplier.slice(-1).toLowerCase() === 'x' ?
-            Number(multiplier.slice(0, -1)) :
-            Number(multiplier);
-
-        // We don't pass the multiplier if it's NaN
-        // so it defaults to 1
-        let composite = compositeImages(
-          template,
-          currentTemplate.dimensions,
-          screenshot,
-          foreground,
-          format,
-          !isNaN(computedMultipler) && computedMultipler
-        )
-
-        // Write file
-        writeFile(
-          composite,
-          destination,
-          suffix,
-          format,
-          id
-        );
-      });
-    });
-  }
-
-  render() {
-    const { screenshot } = this.props;
 
     return (
       <button
         ref="exportButton"
-        onClick={() => this.export()}
+        onClick={() => exportAssets(currentTemplate, screenshot, sizes)}
         style={[
           styles.base,
           !screenshot && styles.disabled
