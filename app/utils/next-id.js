594d6997b8a08b5290af253394854a973d639cc5
Tyler Martinez <slightlytyler@gmail.com>
2015-12-22 23:03:13 -0600

    Actions and reducers spec

--- app/utils/next-id.js
+++ app/utils/next-id.js
@@ -2,9 +2,19 @@ import { size, reduce } from 'lodash';
 
 // Takes a set with sequential numerical ideas
 // and returns the next in the sequence
-export default (set) =>
-  size(set) > 1 ?
-  reduce(set, (a, b) =>
-    a.id > b.id ? a : b
-  ).id + 1 :
-  set[Object.keys(set)[0]].id + 1;
+export default function nextId(set) {
+  const hasEntities = size(set) > 0;
+  const hasMultipleEntities = size(set) > 1;
+
+  if (hasEntities) {
+    if (hasMultipleEntities) {
+      return reduce(set, (a, b) =>
+        a.id > b.id ? a : b
+      ).id + 1
+    } else {
+      return 1;
+    }
+  } else {
+    return 0;
+  }
+}
---
+++ test/actions/sizes.spec.js
@@ -0,0 +1,41 @@
+/* eslint no-unused-expressions: 0 */
+import { expect } from 'chai';
+import { spy } from 'sinon';
+import {
+  addSize,
+  removeSize,
+  updateSize
+} from 'pods/sizes/actions';
+import { actionTypes } from 'pods/sizes/constants';
+const {
+  ADD_SIZE,
+  REMOVE_SIZE,
+  UPDATE_SIZE
+} = actionTypes;
+
+
+describe('actions', () => {
+  it('addSize should create an ADD_SIZE action', () => {
+    expect(addSize()).to.deep.equal({ type: ADD_SIZE });
+  });
+
+  it('removeSize should create an REMOVE_SIZE action', () => {
+    let id = 1;
+
+    expect(removeSize(id)).to.deep.equal({
+      type: REMOVE_SIZE,
+      id
+    });
+  });
+
+  it('updateSize should create an UPDATE_SIZE action', () => {
+    let id = 1;
+    let props = { test: 'test' };
+
+    expect(updateSize(id, props)).to.deep.equal({
+      type: UPDATE_SIZE,
+      id,
+      props
+    });
+  });
+});
