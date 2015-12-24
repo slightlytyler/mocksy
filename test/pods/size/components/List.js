1e4481a63751d220f9ea07cc55008b4415dd05de
Tyler Martinez <slightlytyler@gmail.com>
2015-12-24 13:04:38 -0600

    Better tests for SizeList component

--- test/pods/size/components/List.js
+++ test/pods/size/components/List.js
@@ -36,6 +36,8 @@ describe('List', () => {
   const tree = sd.shallowRender(React.createElement(SizeList, props));
   const vdom = tree.getRenderOutput();
   const items = vdom.props.children.filter(item => isElementOfType(item, SizeItem));
+  const item = items[0];
+  const lastItem = items[1];
 
   it('should pass propTypes check', () => {
     assert(!errorStub.called, `\n${errorStub.args.join('\n')}`);
@@ -43,5 +45,51 @@ describe('List', () => {
 
   it('should display a list of SizeItem components', () => {
     expect(items.length).to.equal(size(props.sizes));
+  });
+
+  it('should pass correct props to SizeItem', () => {
+    let expectedProps = {
+      ...props.sizes[0],
+      isOnlySize: false,
+      isLastSize: false,
+      removeSize: actions.removeSize,
+      updateSize: actions.updateSize
+    };
+
+    expect(item.props).to.deep.equal(expectedProps);
+  });
+
+  it('should pass isLastSize as true for last size item', () => {
+    let expectedProps = {
+      ...props.sizes[1],
+      isOnlySize: false,
+      isLastSize: true,
+      removeSize: actions.removeSize,
+      updateSize: actions.updateSize
+    };
+
+    expect(lastItem.props).to.deep.equal(expectedProps);
+  });
+
+  it('should pass isOnlySize and isLastSize as true for only size item', () => {
+    let onlySizeProps = {
+      sizes: {
+        0: props.sizes[0]
+      },
+
+      ...actions
+    };
+    let onlySizeTree = sd.shallowRender(React.createElement(SizeList, onlySizeProps));
+    let onlySizeVdom = onlySizeTree.getRenderOutput();
+    let onlySizeItem = onlySizeVdom.props.children.filter(item => isElementOfType(item, SizeItem))[0];
+    let expectedProps = {
+      ...onlySizeProps.sizes[0],
+      isOnlySize: true,
+      isLastSize: true,
+      removeSize: actions.removeSize,
+      updateSize: actions.updateSize
+    };
+
+    expect(onlySizeItem.props).to.deep.equal(expectedProps);
   });
 });
