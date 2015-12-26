171c39cfa49e38d01a05d4bb976f76de2f60e0c3
Tyler Martinez <slightlytyler@gmail.com>
2015-12-26 12:34:22 -0600

    Screenshot tests

---
+++ test/pods/screenshots/reducers.js
@@ -0,0 +1,32 @@
+import { expect } from 'chai';
+import reducer from 'pods/screenshots/reducers';
+import { setCurrentScreenshot } from 'pods/screenshots/actions';
+
+const initialState = {
+  condition: {},
+  entities: {}
+};
+
+const testPath = '/path/to/test';
+
+const testState = {
+  condition: {
+    currentScreenshot: testPath
+  },
+  entities: {}
+};
+
+export default describe('reducers', () => {
+  it('should handle initial state', () => {
+    expect(reducer(undefined, {})).to.deep.equal(initialState);
+  });
+
+  it('should handle SET_CURRENT_SCREENSHOT', () => {
+    expect(reducer(initialState, setCurrentScreenshot(testPath))).to.deep.equal(testState);
+  });
+
+  it('should handle unknown action type', () => {
+    expect(reducer(initialState, { type: 'unknown' })).to.deep.equal(initialState);
+    expect(reducer(testState, { type: 'unknown' })).to.deep.equal(testState);
+  });
+});
