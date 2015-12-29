da399b383f21a8dc8768eb599e9b0d552fd4fb80
Tyler Martinez <slightlytyler@gmail.com>
2015-12-29 14:14:26 -0600

    A better way for next-id

--- app/utils/next-id.js
+++ app/utils/next-id.js
@@ -4,16 +4,11 @@ import { size, reduce } from 'lodash';
 // and returns the next in the sequence
 export default function nextId(set) {
   const hasEntities = size(set) > 0;
-  const hasMultipleEntities = size(set) > 1;
 
   if (hasEntities) {
-    if (hasMultipleEntities) {
-      return reduce(set, (a, b) =>
-        a.id > b.id ? a : b
-      ).id + 1
-    } else {
-      return set[Object.keys(set)[0]].id + 1;
-    }
+    return reduce(set, (a, b) =>
+      a.id > b.id ? a : b
+    ).id + 1
   } else {
     return 0;
   }
